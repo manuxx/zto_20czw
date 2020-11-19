021de5ead8c51add587f2d1644e8e20e56b9e2a7
praecipue <43908213+praecipue@users.noreply.github.com>
2020-11-19 15:24:34 +0100

    Check pets name whe trying to add
    Does not implement Equals based on name

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -19,6 +19,11 @@ namespace Training.DomainClasses
 
         public void Add(Pet newPet)
         {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.name == newPet.name)
+                    return;
+            }
             if (_petsInTheStore.IndexOf(newPet) == -1)
                 _petsInTheStore.Add(newPet);
         }
