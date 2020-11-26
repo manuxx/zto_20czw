21a6e0340f06713225d7e7792e94471110037a3b
praecipue <43908213+praecipue@users.noreply.github.com>
2020-11-26 22:44:57 +0100

    AllMice implemented

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -38,11 +38,62 @@ namespace Training.DomainClasses
             }
         }
 
+        private IEnumerable<Pet> All(Predicate<Pet> pred)
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pred(pet))
+                {
+                    yield return pet;
+                }
+            }
+        }
+
         public IEnumerable<Pet> AllPetsSortedByName()
         {
             var list = new List<Pet>(_petsInTheStore);
             list.Sort((pet, pet1) => pet.name.CompareTo(pet1.name));
             return list;
+        }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            return All(pet => pet.species == Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            throw new NotImplementedException();
         }
     }
 }
