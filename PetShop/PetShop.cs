c9f55649627040aa7bce364f4772ebc4b0b37834
filip-chodziutko <filipchodziutko@gmail.com>
2020-11-26 15:18:24 +0100

    Generic read only wrapper in separate file

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace Training.DomainClasses
@@ -15,7 +14,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            return new ReadOnlySet(_petsInTheStore); //_petsInTheStore.OneAtATime();
+            return new ReadOnlyWrapper<Pet>(_petsInTheStore); //_petsInTheStore.OneAtATime();
         }
 
         public void Add(Pet newPet)
@@ -26,29 +25,6 @@ namespace Training.DomainClasses
                     return;
             } 
             _petsInTheStore.Add(newPet);
-        }
-    }
-
-    public class ReadOnlySet : IEnumerable<Pet>
-    {
-        private readonly IList<Pet> _petsInTheStore;
-
-        public ReadOnlySet(IList<Pet> petsInTheStore)
-        {
-            _petsInTheStore = petsInTheStore;
-        }
-
-        public IEnumerator<Pet> GetEnumerator()
-        {
-            foreach (var pet in _petsInTheStore)
-            {
-                yield return pet;
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
         }
     }
 }
