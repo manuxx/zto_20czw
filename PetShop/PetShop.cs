9825474204596db65632098ec2ec1eaac7bc10ec
Jakub Lewkowicz <jacob.lewkowicz@gmail.com>
2020-11-26 15:02:31 +0100

    Add extension class

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Training.DomainClasses
@@ -14,10 +15,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            foreach (var pet in _petsInTheStore)
-            {
-                yield return pet;
-            }
+            return EnumerableExt.OneAtATime(_petsInTheStore);
         }
 
         public void Add(Pet newPet)
