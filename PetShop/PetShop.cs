00c0768f54f67e67366b80d0496026042d255d1e
maciej siudek <siudekmaciek12@gmail.com>
2020-11-26 19:11:34 +0100

    [171896] homework

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,34 +1,29 @@
 using System;
 using System.Collections.Generic;
 
-namespace Training.DomainClasses
-{
-    public class PetShop
-    {
+namespace Training.DomainClasses{
+    public class PetShop{
         private IList<Pet> _petsInTheStore;
 
-        public PetShop(IList<Pet> petsInTheStore)
-        {
+        public PetShop(IList<Pet> petsInTheStore){
             this._petsInTheStore = petsInTheStore;
         }
 
-        public IEnumerable<Pet> AllPets()
-        {
+        public IEnumerable<Pet> AllPets(){
             return new ReadOnlyWrapper<Pet>(_petsInTheStore);
         }
 
-        public void Add(Pet newPet)
-        {
+        public void Add(Pet newPet){
             foreach (var pet in _petsInTheStore)
             {
                 if (pet.name == newPet.name)
                     return;
-            } 
+            }
+
             _petsInTheStore.Add(newPet);
         }
 
-        public IEnumerable<Pet> AllCats()
-        {
+        public IEnumerable<Pet> AllCats(){
             foreach (var pet in _petsInTheStore)
             {
                 if (pet.species == Species.Cat)
@@ -38,11 +33,58 @@ namespace Training.DomainClasses
             }
         }
 
-        public IEnumerable<Pet> AllPetsSortedByName()
-        {
+        public IEnumerable<Pet> AllPetsSortedByName(){
             var list = new List<Pet>(_petsInTheStore);
             list.Sort((pet, pet1) => pet.name.CompareTo(pet1.name));
             return list;
+        }
+
+        public IEnumerable<Pet> AllMice(){
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Mouse)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllFemalePets(){
+            foreach (var pet in _petsInTheStore)
+                if (pet.sex == Sex.Female)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs(){
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Cat || pet.species == Species.Dog)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice(){
+            foreach (var pet in _petsInTheStore)
+                if (pet.species != Species.Mouse)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010(){
+            foreach (var pet in _petsInTheStore)
+                if (pet.yearOfBirth > 2010)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010(){
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Dog && pet.yearOfBirth > 2010)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllMaleDogs(){
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Dog && pet.sex == Sex.Male)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits(){
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Rabbit || pet.yearOfBirth > 2011)
+                    yield return pet;
         }
     }
 }
