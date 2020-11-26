6cd8b4f463ffa122d58e23fa9908635883deed41
Maciej Stanuch <maciejstanuch.ms@gmail.com>
2020-11-26 15:09:04 +0100

    ReadOnlySet

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Training.DomainClasses
@@ -14,7 +15,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            return _petsInTheStore.OneAtATime();
+            return new ReadOnlySet(_petsInTheStore);
         }
 
         public void Add(Pet newPet)
@@ -25,6 +26,29 @@ namespace Training.DomainClasses
                     return;
             } 
             _petsInTheStore.Add(newPet);
+        }
+    }
+
+    public class ReadOnlySet : IEnumerable<Pet>
+    {
+        private readonly IEnumerable<Pet> _pets;
+
+        public ReadOnlySet(IEnumerable<Pet> pets)
+        {
+            _pets = pets;
+        }
+
+        public IEnumerator<Pet> GetEnumerator()
+        {
+            foreach (var pet in _pets)
+            {
+                yield return pet;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
