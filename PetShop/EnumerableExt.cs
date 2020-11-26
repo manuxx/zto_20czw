b6de6d62c9ed182fca852415a9387bc1b6bb1fa7
Nooakk <kapseleczekk@gmail.com>
2020-11-26 15:24:26 +0100

    Dodanie plików

---
+++ PetShop/EnumerableExt.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Training.DomainClasses
+{
+    static internal class EnumerableExt
+    {
+        public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
+        {
+            foreach (var item in items)
+                yield return item;
+        }
+    }
+}
