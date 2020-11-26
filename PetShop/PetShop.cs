cc9896f8938b6398182ebd1781a4280bb5c81296
praecipue <43908213+praecipue@users.noreply.github.com>
2020-11-26 15:08:22 +0100

    ReadOnlySet Enumerator

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Training.DomainClasses
@@ -14,7 +15,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            return _petsInTheStore.OneAtATime();
+            return new ReadOnlySet(_petsInTheStore);
         }
 
         public void Add(Pet newPet)
@@ -25,6 +26,26 @@ namespace Training.DomainClasses
                     return;
             } 
             _petsInTheStore.Add(newPet);
+        }
+    }
+
+    public class ReadOnlySet : IEnumerable<Pet>
+    {
+        private readonly IList<Pet> _petsInTheStore;
+
+        public ReadOnlySet(IList<Pet> petsInTheStore)
+        {
+            _petsInTheStore = petsInTheStore;
+        }
+
+        public IEnumerator<Pet> GetEnumerator()
+        {
+            return _petsInTheStore.OneAtATime().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _petsInTheStore.OneAtATime().GetEnumerator();
         }
     }
 }
