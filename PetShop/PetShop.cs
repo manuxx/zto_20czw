000f2fd0c5b07fe8042fc4bb9584aedaeffbe343
Paweł Kochański <pawkoch98@gmail.com>
2020-11-26 17:11:20 +0100

    Revert "zadanie domowe"
    This reverts commit 0ccdb2db65d4e749c265a349e9bf98fb1b8f8b51.

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -11,16 +11,6 @@ namespace Training.DomainClasses
         {
             this._petsInTheStore = petsInTheStore;
         }
-        public IEnumerable<Pet> AllPets(Func<Pet, bool> filter)
-        {
-            foreach (Pet pet in _petsInTheStore)
-            {
-                if (filter(pet))
-                {
-                    yield return pet;
-                }
-            }
-        }
 
         public IEnumerable<Pet> AllPets()
         {
@@ -39,7 +29,13 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllCats()
         {
-            return this.AllPets((pet => pet.species == Species.Cat));
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species == Species.Cat)
+                {
+                    yield return pet;
+                }
+            }
         }
 
         public IEnumerable<Pet> AllPetsSortedByName()
@@ -47,47 +43,6 @@ namespace Training.DomainClasses
             var list = new List<Pet>(_petsInTheStore);
             list.Sort((pet, pet1) => pet.name.CompareTo(pet1.name));
             return list;
-        }
-
-
-        public IEnumerable<Pet> AllMice()
-        {
-            return this.AllPets(pet => pet.species == Species.Mouse);
-        }
-
-        public IEnumerable<Pet> AllFemalePets()
-        {
-            return this.AllPets(pet => pet.sex == Sex.Female);
-        }
-
-        public IEnumerable<Pet> AllCatsOrDogs()
-        {
-            return this.AllPets(pet => pet.species == Species.Cat || pet.species == Species.Dog);
-        }
-
-        public IEnumerable<Pet> AllPetsButNotMice()
-        {
-            return this.AllPets(pet => pet.species != Species.Mouse);
-        }
-
-        public IEnumerable<Pet> AllPetsBornAfter2010()
-        {
-            return this.AllPets(pet => pet.yearOfBirth > 2010);
-        }
-
-        public IEnumerable<Pet> AllDogsBornAfter2010()
-        {
-            return this.AllPets(pet => pet.species == Species.Dog && pet.yearOfBirth > 2010);
-        }
-
-        public IEnumerable<Pet> AllMaleDogs()
-        {
-            return this.AllPets(pet => pet.species == Species.Dog && pet.sex == Sex.Male);
-        }
-        //Test zosta³ Ÿle napisany
-        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
-        {
-            return this.AllPets(pet => pet.yearOfBirth > 2010 || pet.species==Species.Rabbit);
         }
     }
 }
