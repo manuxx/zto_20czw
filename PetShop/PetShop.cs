d09c706419f46d17706b4a5eb1737bd577839e50
kamil stachera <kamilstachera5@wp.pl>
2020-11-26 21:57:56 +0100

    Homework

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -44,5 +44,61 @@ namespace Training.DomainClasses
             list.Sort((pet, pet1) => pet.name.CompareTo(pet1.name));
             return list;
         }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Mouse)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.sex == Sex.Female)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Cat || pet.species == Species.Dog)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.species != Species.Mouse)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.yearOfBirth > 2010)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Dog && pet.yearOfBirth > 2010)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Dog && pet.sex == Sex.Male)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Rabbit || pet.yearOfBirth > 2011)
+                    yield return pet;
+        }
     }
 }
