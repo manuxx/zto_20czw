4a93c9488f7003bdfa5936300e7619069e06b32a
kamil stachera <kamilstachera5@wp.pl>
2020-11-26 15:43:45 +0100

    Zad 1

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Training.DomainClasses
 {
@@ -15,6 +16,27 @@ namespace Training.DomainClasses
         public IEnumerable<Pet> AllPets()
         {
             return new ReadOnlyWrapper<Pet>(_petsInTheStore);
+        }
+
+        public IEnumerable<Pet> AllCats()
+        {
+
+            var cats = _petsInTheStore;
+
+            for (var i = cats.Count - 1; i >= 0; i--)
+            {
+                if (cats[i].species != Species.Cat)
+                    cats.RemoveAt(i);
+            }
+
+            return new ReadOnlyWrapper<Pet>(cats);
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {
+            var sortedList = _petsInTheStore.ToList();
+            sortedList.Sort((x, y) => string.CompareOrdinal(x.name, y.name));
+            return new ReadOnlyWrapper<Pet>(sortedList);
         }
 
         public void Add(Pet newPet)
