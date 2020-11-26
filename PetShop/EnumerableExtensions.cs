bb9d4a406c6654fb01d2084f612c31ff105514cb
kamil stachera <kamilstachera5@wp.pl>
2020-11-26 15:02:15 +0100

    Zad Ext method

---
+++ PetShop/EnumerableExtensions.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Training.DomainClasses;
+
+internal static class EnumerableExtensions
+{
+    public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
+    {
+        foreach (var item in items)
+        {
+            yield return item;
+        }
+    }
+}
