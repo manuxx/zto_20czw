2223862f37f739cb787ab60f0e56ffc7466633a3
Mikhail Lanchytski <mikhl@amazon.pl>
2020-11-26 16:30:07 +0100

    Add extension

---
+++ PetShop/EnumerableExt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Training.DomainClasses;
+
+static internal class EnumerableExt
+{
+    public static IEnumerable<TItem> OneAtATime<TItem>(IEnumerable<TItem> items)
+    {
+        foreach (var item in items)
+        {
+            yield return item;
+        }
+    }
+}
