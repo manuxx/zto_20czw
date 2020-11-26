fb6c5ce7aa84dd990423ac964c464fdd156b7e84
manuxx <manusxyz@gmail.com>
2020-11-26 14:56:45 +0100

    zmiana na extension methods

--- PetShop/EnumerableExt.cs
+++ PetShop/EnumerableExt.cs
@@ -3,7 +3,7 @@ using Training.DomainClasses;
 
 static internal class EnumerableExt
 {
-    public static IEnumerable<TItem> OneAtATime<TItem>(IEnumerable<TItem> items)
+    public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
     {
         foreach (var item in items)
         {
