f1589e2a882aaf3ac7f6e1322650a195b57cf46a
frupelek <elkacper25@wp.pl>
2020-12-03 15:25:47 +0100

    przeniesione metody tworzące predykaty do Pet

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -29,26 +29,16 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllCats()
         {
-            return _petsInTheStore.AllThat(IsSpecies(Species.Cat));
+            return _petsInTheStore.AllThat(Pet.IsSpeciesOf(Species.Cat));
         }
         public IEnumerable<Pet> AllMice()
         {
-            return _petsInTheStore.AllThat(IsSpecies(Species.Mouse));
-        }
-
-        private static Predicate<Pet> IsSpecies(Species species)
-        {
-            return pet => pet.species == species;
+            return _petsInTheStore.AllThat(Pet.IsSpeciesOf(Species.Mouse));
         }
 
         public IEnumerable<Pet> AllFemalePets()
         {
-            return _petsInTheStore.AllThat(IsFemale());
-        }
-
-        private static Predicate<Pet> IsFemale()
-        {
-            return pet => pet.sex == Sex.Female;
+            return _petsInTheStore.AllThat(Pet.IsFemale());
         }
 
         public IEnumerable<Pet> AllCatsOrDogs()
@@ -58,22 +48,12 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPetsButNotMice()
         {
-            return _petsInTheStore.AllThat(IsNotSpeciesOf(Species.Mouse));
-        }
-
-        private static Predicate<Pet> IsNotSpeciesOf(Species species)
-        {
-            return pet => pet.species != species;
+            return _petsInTheStore.AllThat(Pet.IsNotSpeciesOf(Species.Mouse));
         }
 
         public IEnumerable<Pet> AllPetsBornAfter2010()
         {
-            return _petsInTheStore.AllThat(IsBornAfter(2010));
-        }
-
-        private static Predicate<Pet> IsBornAfter(int year)
-        {
-            return pet => pet.yearOfBirth > year;
+            return _petsInTheStore.AllThat(Pet.IsBornAfter(2010));
         }
 
         public IEnumerable<Pet> AllDogsBornAfter2010()
