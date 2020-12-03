e12cf7cb4e6324453b182a3694f5720c8ad3b2f2
Mikhail Lanchytski <mikhl@amazon.pl>
2020-12-03 15:40:22 +0100

    Pet methods

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -29,17 +29,17 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllCats()
         {
-            return _petsInTheStore.AllThat((pet => pet.species == Species.Cat));
+            return _petsInTheStore.AllThat(Pet.IsSpeciesOf(Species.Cat));
         }
 
         public IEnumerable<Pet> AllMice()
         {
-            return _petsInTheStore.AllThat((pet => pet.species == Species.Mouse));
+            return _petsInTheStore.AllThat(Pet.IsSpeciesOf(Species.Mouse));
         }
 
         public IEnumerable<Pet> AllFemalePets()
         {
-            return _petsInTheStore.AllThat((pet => pet.sex == Sex.Female));
+            return _petsInTheStore.AllThat(Pet.IsFemale());
         }
 
         public IEnumerable<Pet> AllCatsOrDogs()
@@ -49,12 +49,12 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPetsButNotMice()
         {
-            return _petsInTheStore.AllThat((pet => pet.species != Species.Mouse));
+            return _petsInTheStore.AllThat(Pet.IsNotASpeciesOf(Species.Mouse));
         }
 
         public IEnumerable<Pet> AllPetsBornAfter2010()
         {
-            return _petsInTheStore.AllThat((pet => pet.yearOfBirth > 2010));
+            return _petsInTheStore.AllThat(Pet.IsBornAfter(2010));
         }
 
         public IEnumerable<Pet> AllDogsBornAfter2010()
