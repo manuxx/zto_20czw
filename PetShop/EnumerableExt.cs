9f8576d4e5c29f1940cc95f43927a7036374a6dc
qaze <qazedcwsx4@gmail.com>
2020-12-03 15:56:38 +0100

    zadanie

--- PetShop/EnumerableExt.cs
+++ PetShop/EnumerableExt.cs
@@ -4,6 +4,24 @@ using Training.DomainClasses;
 
 static internal class EnumerableExt
 {
+    internal class AnonymousCriteria<TItem> : Criteria<TItem>
+    {
+        private readonly Predicate<TItem> _condition;
+
+ 
+
+        public AnonymousCriteria(Predicate<TItem> condition)
+        {
+            _condition = condition;
+        }
+
+ 
+
+        public bool IsSatisfiedBy(TItem item)
+        {
+            return _condition(item);
+        }
+    }
     public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
     {
         foreach (var item in items)
