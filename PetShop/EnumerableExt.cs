f10573554ed49e87a54f9e72d8539d0f7a7dde80
konrad-sztenderski <sztenk07@gmail.com>
2020-12-03 14:48:01 +0100

    Extract extension method

--- PetShop/EnumerableExt.cs
+++ PetShop/EnumerableExt.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Training.DomainClasses;
 
@@ -9,5 +10,11 @@ static internal class EnumerableExt
         {
             yield return item;
         }
+    }
+
+    public static IEnumerable<Pet> AllThat(this IList<Pet> pets, Predicate<Pet> condition) {
+        foreach (var pet in pets)
+            if (condition(pet))
+                yield return pet;
     }
 }
