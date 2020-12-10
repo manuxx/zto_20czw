4ee64f6f6bfec2481bf99991f044ca567fb947ee
manuxx <manusxyz@gmail.com>
2020-12-10 15:37:31 +0100

    and

---
+++ PetShop/CriteriaExtension.cs
@@ -0,0 +1,10 @@
+namespace Training.DomainClasses
+{
+    public static class CriteriaExtension
+    {
+        public static Criteria<TItem> And<TItem>(this Criteria<TItem> leftCriteria, Criteria<TItem> rightCriteria)
+        {
+            return new Conjuction<TItem>(leftCriteria, rightCriteria);
+        }
+    }
+}
