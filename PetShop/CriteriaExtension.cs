a4665e11ddf00813143af179326e1e065937137f
Jan P <jpomirski@gmail.com>
2020-12-10 15:34:14 +0100

    Create Add extension method

---
+++ PetShop/CriteriaExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Training.DomainClasses;
+
+static internal class CriteriaExtension
+{
+    public static Criteria<TItem> And<TItem>(this Criteria<TItem> leftCriteria, Criteria<TItem> rightCriteria)
+    {
+        return new Conjuction<TItem>(leftCriteria, rightCriteria);
+    }
+}
