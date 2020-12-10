03c85b3546663f6a74337eae6d6df968a23166c0
Pan Jliszka <pliszka.jan98@gmail.com>
2020-12-10 14:53:06 +0100

    Generic in new file

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -75,18 +75,4 @@ namespace Training.DomainClasses
             }
         }
     }
-
-    public class Negation<T> : Criteria<Pet>
-    {
-        private Criteria<Pet> _criteria;
-        public Negation(Criteria<Pet> criteria)
-        {
-            _criteria = criteria;
-        }
-
-        public bool IsSatisfiedBy(Pet item)
-        {
-            return !_criteria.IsSatisfiedBy(item);
-        }
-    }
 }
