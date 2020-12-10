8c23e1f51f1fd1d76b2d795cec9e2ad9ceab59fa
qaze <qazedcwsx4@gmail.com>
2020-12-10 15:00:41 +0100

    alternative

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -79,5 +79,19 @@ namespace Training.DomainClasses
             list.Sort((pet, pet1) => pet.name.CompareTo(pet1.name));
             return list;
         }
+        
+        public class Alternative<T> : Criteria<T> {
+            private readonly Criteria<T> _criteria1;
+            private readonly Criteria<T> _criteria2;
+
+            public Alternative(Criteria<T> criteria1, Criteria<T> criteria2) {
+                _criteria1 = criteria1;
+                _criteria2 = criteria2;
+            }
+
+            public bool IsSatisfiedBy(T item) {
+                return _criteria1.IsSatisfiedBy(item) || _criteria2.IsSatisfiedBy(item);
+            }
+        }
     }
 }
