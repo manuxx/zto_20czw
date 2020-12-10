6f3dc7f263a7d964de83098293bccb6a5f1ea9b3
maciej siudek <siudekmaciek12@gmail.com>
2020-12-10 14:45:15 +0100

    add negation

---
+++ PetShop/Negation.cs
@@ -0,0 +1,13 @@
+﻿namespace PetShop{
+    public class Negation<T> : Criteria<T> {
+        private Criteria<T> _criteria;
+
+        public Negation(Criteria<T> criteria) {
+            _criteria = criteria;
+        }
+
+        public bool IsSatisfiedBy(T item) {
+            return !_criteria.IsSatisfiedBy(item);
+        }
+    }
+}
