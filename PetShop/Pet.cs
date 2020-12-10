4569aa304b4c2c643686887010299b5a425ddf0c
Paweł Kochański <pawkoch98@gmail.com>
2020-12-10 14:53:44 +0100

    Negation

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -75,18 +75,4 @@ namespace Training.DomainClasses
             }
         }
     }
-
-    public class Negation<T> : Criteria<T>
-    {
-        private Criteria<T> _criteria;
-        public Negation(Criteria<T> criteria)
-        {
-            _criteria = criteria;
-        }
-
-        public bool IsSatisfiedBy(T item)
-        {
-            return !_criteria.IsSatisfiedBy(item);
-        }
-    }
 }
