7befb734de9bbcc2009147486687472386d874b1
frupelek <elkacper25@wp.pl>
2020-12-10 14:48:58 +0100

    zadanie1 rozwiązane

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -76,5 +76,19 @@ namespace Training.DomainClasses
         }
     }
 
-    
+    public class Negation<TItem> : Criteria<TItem>
+    {
+        private readonly Criteria<TItem> _criteria;
+
+        public Negation(Criteria<TItem> criteria)
+        {
+            _criteria = criteria;
+            
+        }
+
+        public bool IsSatisfiedBy(TItem item)
+        {
+            return !_criteria.IsSatisfiedBy(item);
+        }
+    }
 }
